507124e45d8f6036fa61dc84a39c23402dac0835
Disura Aberathna <disura2005@gmail.com>
2025-07-05 15:32:54 +0530

    Create UserSessionBean.java

---
+++ ejb-module/src/main/java/com/disuraaberathna/ejb/beans/UserSessionBean.java
@@ -0,0 +1,67 @@
+package com.disuraaberathna.ejb.beans;
+
+import com.disuraaberathna.core.model.User;
+import com.disuraaberathna.core.service.UserService;
+import com.disuraaberathna.core.util.Encryptor;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
+import java.util.Optional;
+
+@Stateless
+public class UserSessionBean implements UserService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public boolean validate(String username, String password) {
+        try {
+            User user = em.createNamedQuery("Customer.findByUsername", User.class).setParameter("username", username).getSingleResult();
+
+            return user != null && Encryptor.checkPassword(password, user.getPassword());
+        } catch (NoResultException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Optional<User> getUserByUsername(String username) {
+        try {
+            TypedQuery<User> query = em.createNamedQuery("User.findByUsername", User.class).setParameter("username", username);
+            return Optional.ofNullable(query.getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        try {
+            TypedQuery<User> query = em.createNamedQuery("User.findByEmail", User.class).setParameter("email", email);
+            return Optional.ofNullable(query.getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    @RolesAllowed({"USER", "ADMIN", "SUPER_ADMIN"})
+    public void updateUser(User user) {
+        em.merge(user);
+    }
+
+    @Override
+    @RolesAllowed({"USER", "ADMIN", "SUPER_ADMIN"})
+    public void deleteUser(User user) {
+        em.remove(user);
+    }
+}
