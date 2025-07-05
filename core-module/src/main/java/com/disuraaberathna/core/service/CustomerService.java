36c78b87a693fa75f5bf141f37c66a576bb12477
Disura Aberathna <disura2005@gmail.com>
2025-07-05 14:29:09 +0530

    Update CustomerService.java

--- core-module/src/main/java/com/disuraaberathna/core/service/CustomerService.java
+++ core-module/src/main/java/com/disuraaberathna/core/service/CustomerService.java
@@ -3,11 +3,17 @@ package com.disuraaberathna.core.service;
 import com.disuraaberathna.core.model.Customer;
 import jakarta.ejb.Remote;
 
+import java.util.Optional;
+
 @Remote
 public interface CustomerService {
     boolean validate(String username, String password);
-    Customer getCustomerByUsername(String username);
-    Customer getCustomerById(String id);
+
+    Optional<Customer> getCustomerByUsername(String username);
+
+    Optional<Customer> getCustomerById(String id);
+
     void addCustomer(Customer customer);
+
     void updateCustomer(Customer customer);
 }
