ff4a6f65c67c595f6179b4d1e0f1ecf6113fd63f
Disura Aberathna <disura2005@gmail.com>
2025-07-05 15:48:52 +0530

    Update AuthMechanism.java

--- frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
+++ frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
@@ -1,5 +1,6 @@
 package com.disuraaberathna.web.security;
 
+import com.disuraaberathna.core.exception.CommonException;
 import jakarta.inject.Inject;
 import jakarta.security.enterprise.AuthenticationException;
 import jakarta.security.enterprise.AuthenticationStatus;
@@ -10,6 +11,8 @@ import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import jakarta.security.enterprise.identitystore.IdentityStore;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class AuthMechanism implements HttpAuthenticationMechanism {
     @Inject
@@ -26,6 +29,14 @@ public class AuthMechanism implements HttpAuthenticationMechanism {
                 return context.notifyContainerAboutLogin(result);
             } else {
                 return AuthenticationStatus.SEND_FAILURE;
+            }
+        }
+
+        if (context.isProtected() && context.getCallerPrincipal() == null) {
+            try {
+                response.sendRedirect(request.getContextPath() + "/login.jsp");
+            } catch (IOException e) {
+                throw new CommonException("Can not redirect to the login page!");
             }
         }
 
