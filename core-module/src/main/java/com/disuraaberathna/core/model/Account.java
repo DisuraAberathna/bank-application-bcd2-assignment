274fcc184f17076ff107ab398a44b0ada785cba8
Disura Aberathna <disura2005@gmail.com>
2025-07-05 11:56:29 +0530

    created account model

---
+++ core-module/src/main/java/com/disuraaberathna/core/model/Account.java
@@ -0,0 +1,83 @@
+package com.disuraaberathna.core.model;
+
+import com.disuraaberathna.core.enums.Status;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "accounts")
+@Cacheable
+public class Account implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+    private Double balance;
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Account() {
+    }
+
+    public Account(Customer customer, Double balance, String accountNumber) {
+        this.customer = customer;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
