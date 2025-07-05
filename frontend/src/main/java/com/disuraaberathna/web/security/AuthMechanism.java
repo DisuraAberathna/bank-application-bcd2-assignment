d2dbf64e4ceb02fa0eeea047583f79285eaafafc
Disura Aberathna <disura2005@gmail.com>
2025-07-05 14:51:31 +0530

    Update AuthMechanism.java

--- frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
+++ frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
@@ -1,15 +1,33 @@
 package com.disuraaberathna.web.security;
 
+import jakarta.inject.Inject;
 import jakarta.security.enterprise.AuthenticationException;
 import jakarta.security.enterprise.AuthenticationStatus;
+import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
 import jakarta.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
 import jakarta.security.enterprise.authentication.mechanism.http.HttpMessageContext;
+import jakarta.security.enterprise.identitystore.CredentialValidationResult;
+import jakarta.security.enterprise.identitystore.IdentityStore;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthMechanism implements HttpAuthenticationMechanism {
+    @Inject
+    private IdentityStore identityStore;
+
     @Override
     public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext context) throws AuthenticationException {
+        AuthenticationParameters authParams = context.getAuthParameters();
+
+        if (authParams.getCredential() != null) {
+            CredentialValidationResult result = identityStore.validate(authParams.getCredential());
+
+            if (result.getStatus() == CredentialValidationResult.Status.VALID) {
+                return context.notifyContainerAboutLogin(result);
+            } else {
+                return AuthenticationStatus.SEND_FAILURE;
+            }
+        }
 
         return context.doNothing();
     }
