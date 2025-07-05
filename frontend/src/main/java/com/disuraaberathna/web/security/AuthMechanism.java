f1874d33514d4c7f7cc269ea3153414d0ac010d9
Disura Aberathna <disura2005@gmail.com>
2025-07-05 18:08:25 +0530

    Update AuthMechanism.java

--- frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
+++ frontend/src/main/java/com/disuraaberathna/web/security/AuthMechanism.java
@@ -1,10 +1,12 @@
 package com.disuraaberathna.web.security;
 
 import com.disuraaberathna.core.exception.CommonException;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.security.enterprise.AuthenticationException;
 import jakarta.security.enterprise.AuthenticationStatus;
 import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
+import jakarta.security.enterprise.authentication.mechanism.http.AutoApplySession;
 import jakarta.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
 import jakarta.security.enterprise.authentication.mechanism.http.HttpMessageContext;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
@@ -14,6 +16,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@AutoApplySession
+@ApplicationScoped
 public class AuthMechanism implements HttpAuthenticationMechanism {
     @Inject
     private IdentityStore identityStore;
