402c78ca61d2ef7525354867004de0462b8c33f7
Disura Aberathna <disura2005@gmail.com>
2025-07-05 11:56:58 +0530

    create customer service interface

---
+++ core-module/src/main/java/com/disuraaberathna/core/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.disuraaberathna.core.service;
+
+import com.disuraaberathna.core.model.Customer;
+import jakarta.ejb.Remote;
+
+@Remote
+public interface CustomerService {
+    boolean validate(String username, String password);
+    Customer getCustomerByUsername(String username);
+    Customer getCustomerById(String id);
+}
