a9c33c18d2c76514b4a7cd2b7ed4f1754a123c00
Disura Aberathna <disura2005@gmail.com>
2025-07-06 18:54:50 +0530

    Create Env.java

---
+++ core-module/src/main/java/com/disuraaberathna/core/util/Env.java
@@ -0,0 +1,21 @@
+package com.disuraaberathna.core.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Env {
+    private static final Properties PROPS = new Properties();
+
+    static {
+        try {
+            InputStream in = Env.class.getClassLoader().getResourceAsStream("config.properties");
+            PROPS.load(in);
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static String get(String key) {
+        return PROPS.getProperty(key);
+    }
+}
