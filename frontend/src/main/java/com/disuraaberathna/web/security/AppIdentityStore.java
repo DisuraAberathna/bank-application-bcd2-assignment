67e27f55f857507a47ecbc098c5aee5bea4eb040
Disura Aberathna <disura2005@gmail.com>
2025-07-06 23:50:27 +0530

    Update AppIdentityStore.java

--- frontend/src/main/java/com/disuraaberathna/web/security/AppIdentityStore.java
+++ frontend/src/main/java/com/disuraaberathna/web/security/AppIdentityStore.java
@@ -6,7 +6,6 @@ import com.disuraaberathna.core.service.CustomerService;
 import com.disuraaberathna.core.service.UserService;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.security.enterprise.authentication.mechanism.http.AutoApplySession;
 import jakarta.security.enterprise.credential.Credential;
 import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
