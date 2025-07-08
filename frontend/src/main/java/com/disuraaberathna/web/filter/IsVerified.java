2109628fa9613c249a50fdbcb09e88c0e2e1605e
Disura Aberathna <disura2005@gmail.com>
2025-07-08 12:48:03 +0530

    Update IsVerified.java

--- frontend/src/main/java/com/disuraaberathna/web/filter/IsVerified.java
+++ frontend/src/main/java/com/disuraaberathna/web/filter/IsVerified.java
@@ -2,15 +2,24 @@ package com.disuraaberathna.web.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebFilter({"/verify-email.jsp"})
+@WebFilter({"/verify-email.jsp", "/account/*"})
 public class IsVerified implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (request.getAttribute("verified") != null && (Boolean) request.getAttribute("verified")) {
-            request.getRequestDispatcher("/auth/login.jsp").forward(request, response);
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        if (req.getSession().getAttribute("verified") != null && (Boolean) req.getSession().getAttribute("verified")) {
+            resp.sendRedirect(req.getContextPath() + "/auth/login.jsp");
+        }
+
+        if (request.getAttribute("allow-verified") != null && (Boolean) request.getAttribute("allow-verified")) {
+            request.getRequestDispatcher("/verify-email.jsp").forward(request, response);
         }
 
         chain.doFilter(request, response);
