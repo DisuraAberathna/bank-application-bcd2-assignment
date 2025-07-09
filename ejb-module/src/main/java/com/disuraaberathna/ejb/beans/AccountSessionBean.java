9864b27f97da269624a749a290eed5181b66cf71
Disura Aberathna <disura2005@gmail.com>
2025-07-09 22:53:55 +0530

    Create AccountSessionBean.java

---
+++ ejb-module/src/main/java/com/disuraaberathna/ejb/beans/AccountSessionBean.java
@@ -0,0 +1,31 @@
+package com.disuraaberathna.ejb.beans;
+
+import com.disuraaberathna.core.model.Account;
+import com.disuraaberathna.core.model.Customer;
+import com.disuraaberathna.core.service.AccountService;
+import com.disuraaberathna.core.service.CustomerService;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.UUID;
+
+@Stateless
+public class AccountSessionBean implements AccountService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @EJB
+    private CustomerService customerService;
+
+    @Override
+    public void addAccount(Double deposit, String email) {
+        String accountNumber = UUID.randomUUID().toString();
+
+        Customer customer = customerService.getCustomerByEmail(email);
+        Account account = new Account(customer, deposit, accountNumber);
+
+        em.persist(account);
+    }
+}
