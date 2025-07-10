0b4c0fe0a6f93bc6ed5457605828068670f70e15
Disura Aberathna <disura2005@gmail.com>
2025-07-10 16:03:59 +0530

    Update AccountSessionBean.java

--- ejb-module/src/main/java/com/disuraaberathna/ejb/beans/AccountSessionBean.java
+++ ejb-module/src/main/java/com/disuraaberathna/ejb/beans/AccountSessionBean.java
@@ -5,12 +5,12 @@ import com.disuraaberathna.core.model.Customer;
 import com.disuraaberathna.core.service.AccountService;
 import com.disuraaberathna.core.service.CustomerService;
 import com.disuraaberathna.core.util.AccountNumberGenerator;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-
-import java.util.UUID;
 
 @Stateless
 public class AccountSessionBean implements AccountService {
@@ -21,6 +21,7 @@ public class AccountSessionBean implements AccountService {
     private CustomerService customerService;
 
     @Override
+    @RolesAllowed({"USER", "ADMIN", "SUPER_ADMIN"})
     public void addAccount(Double deposit, String email) {
         String accountNumber = AccountNumberGenerator.generate();
 
@@ -28,5 +29,17 @@ public class AccountSessionBean implements AccountService {
         Account account = new Account(customer, deposit, accountNumber);
 
         em.persist(account);
+    }
+
+    @Override
+    @RolesAllowed({"USER", "ADMIN", "SUPER_ADMIN"})
+    public void updateAccount(Account account) {
+        em.merge(account);
+    }
+
+    @Override
+    @DenyAll
+    public void deleteAccount(Account account) {
+        em.remove(account);
     }
 }
