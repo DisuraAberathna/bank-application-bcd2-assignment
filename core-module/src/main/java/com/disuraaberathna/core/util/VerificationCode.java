383557ae1368fa9a2efc6a0e8686f98668393600
Disura Aberathna <disura2005@gmail.com>
2025-07-10 12:03:50 +0530

    Delete VerificationCode.java

--- core-module/src/main/java/com/disuraaberathna/core/util/VerificationCode.java
+++
@@ -1,9 +0,0 @@
-package com.disuraaberathna.core.util;
-
-import java.util.UUID;
-
-public class VerificationCode {
-    public static String generate() {
-        return UUID.randomUUID().toString();
-    }
-}
