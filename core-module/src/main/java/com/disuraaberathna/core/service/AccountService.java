fb838523b7f8c088aeb705df8f7854a130f5091d
Disura Aberathna <disura2005@gmail.com>
2025-07-10 16:03:49 +0530

    Update AccountService.java

--- core-module/src/main/java/com/disuraaberathna/core/service/AccountService.java
+++ core-module/src/main/java/com/disuraaberathna/core/service/AccountService.java
@@ -6,4 +6,8 @@ import jakarta.ejb.Remote;
 @Remote
 public interface AccountService {
     void addAccount(Double deposit, String email);
+
+    void updateAccount(Account account);
+
+    void deleteAccount(Account account);
 }
