6a0b6c188077112e07d20768eb3471f22e3b0934
Disura Aberathna <disura2005@gmail.com>
2025-07-11 15:40:35 +0530

    Update LogOut.java

--- frontend/src/main/java/com/disuraaberathna/web/action/LogOut.java
+++ frontend/src/main/java/com/disuraaberathna/web/action/LogOut.java
@@ -1,6 +1,9 @@
 package com.disuraaberathna.web.action;
 
+import jakarta.annotation.security.DeclareRoles;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +11,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@DeclareRoles({"CUSTOMER", "USER", "ADMIN"})
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"CUSTOMER", "USER", "ADMIN"}))
 @WebServlet("/auth/logout")
 public class LogOut extends HttpServlet {
     @Override
