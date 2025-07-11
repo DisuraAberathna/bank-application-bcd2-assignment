e260df6d8a7c61506fc62b7f1dac1d8934b1408b
Disura Aberathna <disura2005@gmail.com>
2025-07-11 15:39:38 +0530

    Create AccountDTO.java

---
+++ core-module/src/main/java/com/disuraaberathna/core/dto/AccountDTO.java
@@ -0,0 +1,40 @@
+package com.disuraaberathna.core.dto;
+
+public class AccountDTO {
+    private String accountNumber;
+    private String accountType;
+    private Double balance;
+
+    public AccountDTO() {
+    }
+
+    public AccountDTO(String accountNumber, String accountType, Double balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
