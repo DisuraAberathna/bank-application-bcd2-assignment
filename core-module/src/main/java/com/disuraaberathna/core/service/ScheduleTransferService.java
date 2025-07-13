b38f21e02839d5c2c7c12a2386ad4b30a877a9ca
Disura Aberathna <disura2005@gmail.com>
2025-07-13 20:32:05 +0530

    Update ScheduleTransferService.java

--- core-module/src/main/java/com/disuraaberathna/core/service/ScheduleTransferService.java
+++ core-module/src/main/java/com/disuraaberathna/core/service/ScheduleTransferService.java
@@ -1,12 +1,16 @@
 package com.disuraaberathna.core.service;
 
+import com.disuraaberathna.core.model.ScheduledTransfer;
 import jakarta.ejb.Remote;
 
 import java.util.Date;
+import java.util.List;
 
 @Remote
 public interface ScheduleTransferService {
     Long scheduleTransfer(String fromAccountNo, String toAccountNo, double amount, Date date, String otp);
 
     boolean scheduleTransferConfirm(Long id, String otp);
+
+    List<ScheduledTransfer> getScheduledTransfers();
 }
