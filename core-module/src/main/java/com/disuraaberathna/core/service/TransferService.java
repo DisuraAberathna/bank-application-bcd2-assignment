ad917bb116958cf40fc9f8ee55c44b956a6b18f5
Disura Aberathna <disura2005@gmail.com>
2025-07-13 21:52:41 +0530

    Update TransferService.java

--- core-module/src/main/java/com/disuraaberathna/core/service/TransferService.java
+++ core-module/src/main/java/com/disuraaberathna/core/service/TransferService.java
@@ -1,5 +1,6 @@
 package com.disuraaberathna.core.service;
 
+import com.disuraaberathna.core.model.ScheduledTransfer;
 import jakarta.ejb.Remote;
 
 @Remote
@@ -7,4 +8,6 @@ public interface TransferService {
     Long transfer(String fromAccountNo, String toAccountNo, double amount, String otp);
 
     boolean transferConfirm(Long id, String otp);
+
+    void saveScheduledTransfer(ScheduledTransfer scheduledTransfer);
 }
