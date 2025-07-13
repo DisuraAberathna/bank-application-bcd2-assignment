914a3a6b22a401175c2e7a3d1ac3b5a54154e186
Disura Aberathna <disura2005@gmail.com>
2025-07-13 12:48:35 +0530

    Update ScheduledTransfer.java

--- core-module/src/main/java/com/disuraaberathna/core/model/ScheduledTransfer.java
+++ core-module/src/main/java/com/disuraaberathna/core/model/ScheduledTransfer.java
@@ -1,6 +1,92 @@
 package com.disuraaberathna.core.model;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "schedule_transfers")
 public class ScheduledTransfer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "from_account_id", nullable = false)
+    private Account fromAccount;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "to_account_id", nullable = false)
+    private Account toAccount;
+    private double amount;
+    private String otp;
+    private Date scheduledDate;
+    @Column(updatable = false)
+    private Date createdAt = new Date();
+
+    public ScheduledTransfer() {
+    }
+
+    public ScheduledTransfer(Account fromAccount, Account toAccount, double amount, String otp, Date scheduledDate) {
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amount = amount;
+        this.otp = otp;
+        this.scheduledDate = scheduledDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Account getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(Account fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public Account getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(Account toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public Date getScheduledDate() {
+        return scheduledDate;
+    }
+
+    public void setScheduledDate(Date scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
