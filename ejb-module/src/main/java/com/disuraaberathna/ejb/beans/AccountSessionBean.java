c5e8064a9e1a91a72531570d89af5a5c915e53e9
Disura Aberathna <disura2005@gmail.com>
2025-07-13 12:48:45 +0530

    Update AccountSessionBean.java

--- ejb-module/src/main/java/com/disuraaberathna/ejb/beans/AccountSessionBean.java
+++ ejb-module/src/main/java/com/disuraaberathna/ejb/beans/AccountSessionBean.java
@@ -25,11 +25,13 @@ public class AccountSessionBean implements AccountService {
     private CustomerService customerService;
 
     @Override
+    @RolesAllowed({"CUSTOMER", "USER", "ADMIN", "SUPER_ADMIN"})
     public List<Account> getCustomerAccounts(Customer customer) {
         return em.createNamedQuery("Account.findCustomerAccounts", Account.class).setParameter("customer", customer).getResultList();
     }
 
     @Override
+    @RolesAllowed({"CUSTOMER", "USER", "ADMIN", "SUPER_ADMIN"})
     @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     public Account getAccountByNo(String accountNo) {
         try {
@@ -49,6 +51,36 @@ public class AccountSessionBean implements AccountService {
         Account account = new Account(customer, deposit, accountNumber, type);
 
         em.persist(account);
+    }
+
+    @Override
+    @RolesAllowed({"CUSTOMER"})
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void credit(String accountNo, double amount) {
+        try {
+            Account account = getAccountByNo(accountNo);
+            if (amount > 0) {
+                account.setBalance(account.getBalance() + amount);
+            }
+            em.merge(account);
+        } catch (NullPointerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    @RolesAllowed({"CUSTOMER"})
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void debit(String accountNo, double amount) {
+        try {
+            Account account = getAccountByNo(accountNo);
+            if (account.getBalance() - amount > 1000) {
+                account.setBalance(account.getBalance() - amount);
+            }
+            em.merge(account);
+        } catch (NullPointerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
