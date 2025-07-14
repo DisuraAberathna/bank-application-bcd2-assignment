b7de468e65d875130bbcf7257d94496681ef9d7f
Disura Aberathna <disura2005@gmail.com>
2025-07-14 21:59:23 +0530

    Create Audit.java

---
+++ core-module/src/main/java/com/disuraaberathna/core/annotation/Audit.java
@@ -0,0 +1,12 @@
+package com.disuraaberathna.core.annotation;
+
+import jakarta.interceptor.InterceptorBinding;
+
+import java.lang.annotation.*;
+
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Audit {
+}
