8bf0e37a1d964e2751076fce9cc398ca8c0a6fd9
Disura Aberathna <disura2005@gmail.com>
2025-07-15 22:35:56 +0530

    Update account-dashboard.js

--- frontend/src/main/webapp/public/js/account-dashboard.js
+++ frontend/src/main/webapp/public/js/account-dashboard.js
@@ -498,24 +498,6 @@ const scheduleTransferReset = () => {
     document.getElementById("scheduled-transfer-date-time").value = "";
 };
 
-const formatDate = (dateTime) => {
-    const date = new Date(dateTime);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    const formattedHour = String(hours).padStart(2, '0');
-
-    return `${year}-${month}-${day} ${formattedHour}.${minutes} ${ampm}`;
-}
-
 const showSuccessModal = (title, desc) => {
     document.getElementById("successModalTitle").innerHTML = title;
     document.getElementById("successModalDesc").innerHTML = desc;
