e90c304d1cdb637ff60ef978b0c00c5dbcd247bd
Disura Aberathna <disura2005@gmail.com>
2025-07-20 18:26:23 +0530

    Update CommonException.java

--- core-module/src/main/java/com/disuraaberathna/core/exception/CommonException.java
+++ core-module/src/main/java/com/disuraaberathna/core/exception/CommonException.java
@@ -1,5 +1,8 @@
 package com.disuraaberathna.core.exception;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException(rollback = true)
 public class CommonException extends RuntimeException {
     public CommonException(String message) {
         super(message);
